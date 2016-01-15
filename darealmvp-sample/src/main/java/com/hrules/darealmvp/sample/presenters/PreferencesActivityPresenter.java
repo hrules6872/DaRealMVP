07763ca1f08e019e534f5d04fcc4c8ac3ebd22d7
hector <h.deisidro@gmail.com>
2016-01-15 18:36:36 +0100

    Done PreferencesActivity

--- darealmvp-sample/src/main/java/com/hrules/darealmvp/sample/presenters/PreferencesActivityPresenter.java
+++ darealmvp-sample/src/main/java/com/hrules/darealmvp/sample/presenters/PreferencesActivityPresenter.java
@@ -1,11 +1,21 @@
 package com.hrules.darealmvp.sample.presenters;
 
+import android.preference.Preference;
 import com.hrules.darealmvp.DRPresenter;
 import com.hrules.darealmvp.DRView;
+import com.hrules.darealmvp.sample.R;
+import com.hrules.darealmvp.sample.views.PreferenceActivityView;
 
 public class PreferencesActivityPresenter
     extends DRPresenter<PreferencesActivityPresenter.IPreferencesView> {
 
+  public void onPreferenceClick(Preference preference) {
+    if (preference.getKey().equals(PreferenceActivityView.KEY_PREFS_GOTOREPO)) {
+      view.doGotoRepo(view.getContext().getString(R.string.app_repository_url));
+    }
+  }
+
   public interface IPreferencesView extends DRView {
+    void doGotoRepo(String url);
   }
 }
--- darealmvp-sample/src/main/java/com/hrules/darealmvp/sample/views/PreferenceActivityView.java
+++ darealmvp-sample/src/main/java/com/hrules/darealmvp/sample/views/PreferenceActivityView.java
@@ -1,12 +1,18 @@
 package com.hrules.darealmvp.sample.views;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.Preference;
 import android.support.v7.widget.Toolbar;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.hrules.darealmvp.sample.R;
 import com.hrules.darealmvp.sample.activities.PreferencesActivity;
 
-public class PreferenceActivityView extends PreferencesActivity {
+public class PreferenceActivityView extends PreferencesActivity
+    implements Preference.OnPreferenceClickListener {
+  public static final String KEY_PREFS_GOTOREPO = "prefs_gotoRepo";
+
   @Bind(R.id.toolbar) Toolbar toolbar;
 
   @Override public void initializeViews() {
@@ -16,5 +22,16 @@ public class PreferenceActivityView extends PreferencesActivity {
     getSupportActionBar().setHomeButtonEnabled(true);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+    findPreference(KEY_PREFS_GOTOREPO).setOnPreferenceClickListener(this);
+  }
+
+  @Override public boolean onPreferenceClick(Preference preference) {
+    getPresenter().onPreferenceClick(preference);
+    return true;
+  }
+
+  @Override public void doGotoRepo(String url) {
+    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
   }
 }
